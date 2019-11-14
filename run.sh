fcf36f0b52de51d3403e02439483fa63ebe8e50e
88plug <19512127+88plug@users.noreply.github.com>
2019-11-14 06:58:57 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,23 @@
 echo "Let's get it going..."
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+echo "Updating Manjaro"
+yes | sudo pacman -Syu
+echo "Enable SSH"
 sudo systemctl enable sshd.service; sudo systemctl start sshd.service
+echo "Removing GUI"
 yes | sudo pacman -Rs xfce4 gtkhash-thunar libxfce4ui mousepad orage thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-clipman-plugin xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin parole xfce4-notifyd lightdm light-locker lightdm-gtk-greeter lightdm-gtk-greeter-settings modemmanager
+echo "Make .ssh folder for keys"
 mkdir ~/.ssh 
-sudo pacman -Sy docker docker-compose glances htop bmon jq whois yay ufw fail2ban
+echo "Install goodies | docker docker-compose glances htop bmon jq whois yay ufw fail2ban"
+yes | sudo pacman -Sy docker docker-compose glances htop bmon jq whois yay ufw fail2ban
+echo "Allow SSH"
 sudo ufw allow ssh
+echo "Limit SSH"
 sudo ufw limit ssh
-sudo cat <<EOT > /etc/fail2ban/jail.d/sshd.local
+cat <<EOT > /etc/fail2ban/jail.d/sshd.local
 [sshd]
 enabled   = true
 filter    = sshd
