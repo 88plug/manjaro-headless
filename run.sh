6fc823e6cf3a3dc94f83c97d913aafbe55391a3d
88plug <19512127+88plug@users.noreply.github.com>
2019-11-14 07:01:19 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,19 +4,20 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 echo "Updating Manjaro"
-yes | sudo pacman -Syu
+yes | pacman -Syu
 echo "Enable SSH"
-sudo systemctl enable sshd.service; sudo systemctl start sshd.service
+systemctl enable sshd.service; systemctl start sshd.service
 echo "Removing GUI"
-yes | sudo pacman -Rs xfce4 gtkhash-thunar libxfce4ui mousepad orage thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-clipman-plugin xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin parole xfce4-notifyd lightdm light-locker lightdm-gtk-greeter lightdm-gtk-greeter-settings modemmanager
+yes | pacman -Rs xfce4 gtkhash-thunar libxfce4ui mousepad orage thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-clipman-plugin xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin parole xfce4-notifyd lightdm light-locker lightdm-gtk-greeter lightdm-gtk-greeter-settings modemmanager
 echo "Make .ssh folder for keys"
 mkdir ~/.ssh 
 echo "Install goodies | docker docker-compose glances htop bmon jq whois yay ufw fail2ban"
-yes | sudo pacman -Sy docker docker-compose glances htop bmon jq whois yay ufw fail2ban
+yes | pacman -Sy docker docker-compose glances htop bmon jq whois yay ufw fail2ban
 echo "Allow SSH"
-sudo ufw allow ssh
+ufw allow ssh
 echo "Limit SSH"
-sudo ufw limit ssh
+ufw limit ssh
+echo "Setup jail for naughty SSH attempts"
 cat <<EOT > /etc/fail2ban/jail.d/sshd.local
 [sshd]
 enabled   = true
@@ -27,6 +28,10 @@ maxretry  = 5
 findtime  = 1d
 bantime   = 52w
 EOT
-sudo systemctl start fail2ban.service
-sudo systemctl enable fail2ban.service
+echo "Starting and enabling the jail/fail2ban"
+systemctl start fail2ban.service
+systemctl enable fail2ban.service
+echo "Starting and enabling the docker"
+systemctl start docker.service
+systemctl enable docker.service
 echo "..the going has been got.  Reboot!"
