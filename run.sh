bc74cb9be484905e6afd04e48ce7e99ccdfdfcb8
88plug <19512127+88plug@users.noreply.github.com>
2023-05-22 18:41:57 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -73,7 +73,7 @@ remove_packages() {
 # Check for XFCE4 GUI manager, if found remove it
 if xfce4-panel --version &>/dev/null; then
   echo "Removing XFCE4 GUI"
-  remove_packages xfce4 gtkhash-thunar libxfce4ui mousepad orage thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-clipman-plugin xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xfce4-xkb-plugin parole xfce4-notifyd lightdm light-locker lightdm-gtk-greeter lightdm-gtk-greeter-settings modemmanager
+  remove_packages xfce4 wayland gtkhash-thunar libxfce4ui mousepad orage thunar-archive-plugin thunar-media-tags-plugin xfce4-panel xfce4-battery-plugin xfce4-clipman-plugin xfce4-pulseaudio-plugin xfce4-screenshooter xfce4-whiskermenu-plugin xfce4-xkb-plugin parole xfce4-notifyd lightdm light-locker lightdm-gtk-greeter lightdm-gtk-greeter-settings modemmanager
 else
   echo "XFCE4 not found! No GUI Removed"
 fi
@@ -81,7 +81,7 @@ fi
 # Check for GNOME GUI manager, if found remove it
 if gnome-session --version &>/dev/null; then
   echo "Removing GNOME GUI"
-  remove_packages gnome-shell gnome-terminal gnome-control-center gnome-backgrounds gnome-calculator gnome-disk-utility gnome-keyring gnome-logs gnome-menus gnome-online-accounts gnome-settings-daemon gnome-shell-extensions gnome-software-packagekit-plugin gnome-software packagekit packagekit-qt5 polkit-gnome seahorse vino xdg-user-dirs-gtk
+  remove_packages gnome gnome-session gnome-shell wayland gnome-terminal gnome-control-center gnome-backgrounds gnome-calculator gnome-disk-utility gnome-keyring gnome-logs gnome-menus gnome-online-accounts gnome-settings-daemon gnome-shell-extensions gnome-software-packagekit-plugin gnome-software packagekit packagekit-qt5 polkit-gnome seahorse vino xdg-user-dirs-gtk
 else
   echo "GNOME not found! No GUI Removed"
 fi
